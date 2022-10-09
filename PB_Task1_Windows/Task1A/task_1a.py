2d224c7418f5bf497d271aaf5edb27d10e2698f8
Abhishek S <104844413+ABCET@users.noreply.github.com>
2022-10-09 13:16:13 +0530

    Update task1A
    updated function  detect_medicine_packages(maze_image)
    added arena_parameter for medicine package

--- PB_Task1_Windows/Task1A/task_1a.py
+++ PB_Task1_Windows/Task1A/task_1a.py
@@ -106,7 +106,7 @@ def detect_horizontal_roads_under_construction(maze_image):
 			if (maze_image[i,j]==[255,255,255]).all():
 				horizontal_roads_under_construction.append(chr(64+j//100)+str(i//100)+"-"+chr(65+j//100)+str(i//100))
 					
-   ##################################################
+        ##################################################
 	
 	return horizontal_roads_under_construction	
 
@@ -174,13 +174,62 @@ def detect_medicine_packages(maze_image):
 	---
 	medicine_packages = detect_medicine_packages(maze_image)
 	"""    
-	medicine_packages = []
-
+  	medicine_packages = []
+	
 	##############	ADD YOUR CODE HERE	##############
+	
+    	x=[130,130,170,170]
+    	y=[130,170,130,170]
+    	c=0
+    	for i in range(1,7):
+		shopno='Shop_'+str(i)
+		d=0
+		shape=''
+		dt={"Green":[0,0],"Orange":[0,0],"Pink":[0,0],"Skyblue":[0,0]}
+		for j in range(0,4):
+			color=''
+			if (maze_image[x[j],y[j]]==[0,255,0]).all():
+				color="Green"
+				dt[color]=[x[j],y[j]]
+			elif (maze_image[x[j],y[j]]==[0,127,255]).all():
+				color="Orange"
+				dt[color]=[x[j],y[j]]
+			elif (maze_image[x[j],y[j]]==[180,0,255]).all():
+				color="Pink"
+				dt[color]=[x[j],y[j]]
+			elif (maze_image[x[j],y[j]]==[255,255,0]).all():
+				color="Skyblue"
+				dt[color]=[x[j],y[j]]
+			if color=="Green"or color=="Orange"or color=="Pink"or color=="Skyblue":
+				d+=1
+				if d==1:
+					if j==0 or j==1:
+						cropped_image =maze_image[110:150,110+(i-1)*100+j*40:150+(i-1)*100+j*40]
+				        else:
+						cropped_image =maze_image[150:190,110+(i-1)*100+(j-2)*40:150+(i-1)*100+(j-2)*40]
+				    	imgGrey = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+				   	 _, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY_INV)
+				    	contour,_ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+				   	for cnt in contour:
+						approx = cv2.approxPolyDP(cnt, 0.01* cv2.arcLength(cnt, True), True)
+						if len(approx)==3:
+							shape="Triangle"
+						elif len(approx)==4:
+							shape="Square"
+						else:
+							shape="Circle"
+		    	y[j]+=100 
+		for k in dt:
+			if dt[k]!=[0,0]:
+			medicine_packages.append([shopno,k,shape,dt[k]])     
+		if d>0:
+			c+=1
+			if c>3:
+				break  
+				
+    	return medicine_packages
 
 	##################################################
-
-	return medicine_packages
 
 def detect_arena_parameters(maze_image):
 
@@ -212,11 +261,13 @@ def detect_arena_parameters(maze_image):
 	arena_parameters = detect_arena_parameters(maze_image)
 	"""    
 	arena_parameters = {}
+	
+	##############	ADD YOUR CODE HERE	##############
+	
 	arena_parameters['traffic_signals']=detect_traffic_signals(maze_image)
 	arena_parameters['horizontal_roads_under_construction']=detect_horizontal_roads_under_construction(maze_image)
 	arena_parameters['vertical_roads_under_construction']=detect_vertical_roads_under_construction(maze_image)
-
-	##############	ADD YOUR CODE HERE	##############
+	arena_parameters['medicine_packages']=detect_medicine_packages(maze_image)
 	
 	##################################################
 	
